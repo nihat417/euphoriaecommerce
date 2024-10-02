f29f4a28765ee9c50314b4e8e90d59336af20ad5
nihat417 <akreminihat@gmail.com>
2024-10-02 23:47:51 +0400

    Update

--- src/app/common/Login/page.jsx
+++ src/app/common/Login/page.jsx
@@ -8,22 +8,25 @@ import twitter from '../../../assets/images/twitter.svg';
 const Login = () => {
   return (
     <div className="flex flex-col md:flex-row h-screen">
-      <div className="w-full md:w-1/2">
+      <div className="w-full md:w-1/2 order-2">
         <Image src={lgPhoto} alt="ss" className="w-full h-[50vh] md:h-[100vh] object-cover" />
       </div>
       
-      <div className="w-full md:w-1/2 p-5">
-        <h2 className='text-[#333] ml-[20px] md:ml-[50px] my-[30px] md:my-[50px] text-[30px] md:text-[40px] font-bold'>Sign In Page</h2>
+      <div className="w-full md:w-1/2 p-5 order-1 mb-[50px] md:order-2">
+        <h2 className=' text-[#333] ml-[20px] md:ml-[50px] my-[30px] md:my-[50px] text-[30px] md:text-[40px] font-bold'>Sign In Page</h2>
 
-        <button className='flex items-center justify-center mx-[20px] md:ml-[50px] border-[1px] border-black rounded-[5px] py-[10px] px-5 md:px-10 w-full max-w-md'>
-            <Image src={google} alt='Google' className='mr-[10px]' />
-            Continue with Google
-        </button>
+        <div className='flex flex-col md:mx-[50px]'>
+            <button type="button" className="items-center justify-center  bg-white hover:bg-gray-100 border border-[#000] focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex dark:text-black me-2 mb-2">
+                <Image src={google} alt='Google' className='mr-[10px]' />
+                Continue with Google
+            </button>
 
-        <button className='flex items-center justify-center mt-[20px] mx-[20px] md:ml-[50px] border-[1px] border-black rounded-[5px] py-[10px] px-5 md:px-10 w-full max-w-md'>
-            <Image src={twitter} alt='Twitter' className='mr-[10px]' />
-            Continue with Twitter
-        </button>
+
+            <button type="button" className="items-center justify-center  bg-white hover:bg-gray-100 border border-[#000] focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex  dark:text-black me-2 mb-2">
+                <Image src={twitter} alt='Twitter' className='mr-[10px]' />
+                Continue with Twitter
+            </button>
+        </div>
       </div>
     </div>
   );
